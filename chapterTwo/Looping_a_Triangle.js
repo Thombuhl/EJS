99858c81a4ac264e658b3b0031c04e840a6e1a9e
Thombuhl <thomas.m.buhl@gmail.com>
2021-10-26 17:03:57 -0400

    add chp ex1 - looping triangle

---
+++ chapterTwo/Looping_a_Triangle.js
@@ -0,0 +1,22 @@
+
+
+/* Question: Wrtie as loop that makes seven calls to console.log to
+output the following triangle
+
+//Output
+#
+##
+###
+####
+#####
+######
+#######
+########
+*/
+
+
+
+for (let i = "#"; i.length <= 8; i += "#") {
+    console.log (i);
+};
+
